90a9b82c4916e7d4096cf47eab1e4f14466d939b
WarpedDemon <warped.demon.prince@hotmail.com>
2019-02-14 01:39:31 +0800

    Index
    Thank the lord and his heavenly code magics.

--- index.php
+++ index.php
@@ -2,16 +2,16 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="../Css/LandingPage.css">
-    <link rel="stylesheet" href="../Css/Mobile.css">
-    <link rel="stylesheet" href="../Css/Extras.css">
-    <link rel="stylesheet" href="../Css/animate.css">
+    <link rel="stylesheet" href="Css/LandingPage.css">
+    <link rel="stylesheet" href="Css/Mobile.css">
+    <link rel="stylesheet" href="Css/Extras.css">
+    <link rel="stylesheet" href="Css/animate.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
 
-    <script src="../Js/Master.mjs"></script>
-    <script src="../Js/jquery.min.js"></script>
-    <script src="../Js/wow.min.js"></script>
-    <script src="../Js/jquery-ui.min.js"></script>
+    <script src="Js/Master.mjs"></script>
+    <script src="Js/jquery.min.js"></script>
+    <script src="Js/wow.min.js"></script>
+    <script src="Js/jquery-ui.min.js"></script>
 
     <script type="text/javascript">
       new WOW().init();
@@ -19,11 +19,11 @@
     <title> Nelgara Pty Ltd </title>
   </head>
   <body>
-    <?php include("../Php/Header.php"); ?>
-    <?php include("../Php/Banner.php");
+    <?php include("Php/Header.php"); ?>
+    <?php include("Php/Banner.php");
       $BannerHandlerHelper->CreateNewContent("Commercial Construction - Services");
     ?>
-    <?php include("../Php/MainContent.php");
+    <?php include("Php/MainContent.php");
         $MainContentHandlerHelper->CreateNewContent("cheese", "<b>What We Do</b>", "<h2>Innovative Commercial and Project Management Solutions</h2>
           Nelgara Pty Ltd is an Australian commercial management consulting firm that leverages its significant industry experience to develop and apply best practice solutions to a broad mix of service industries including oil and gas, water treatment, power distribution, infrastructure, resources and mining for both private and public institutions.
           We can understand our clients’ needs quickly, as we have worked there, and respond with significant and cost-effective improvements to their operational performance whilst supporting their business goals.
@@ -36,12 +36,12 @@
         $MainContentHandlerHelper->CreateNewContent("cheese", "<b>Locations</b>", "Nelgara Pty Ltd is based in Perth, WA but consultants have both worked on projects and or being responsible at a business Contracts Manager or Commercial Manager level for projects in Queensland, NSW and Victoria.
           We can also provide solutions through our network of consultant partners in the industry.");
     ?>
-    <?php include("../Php/Footer.php");
+    <?php include("Php/Footer.php");
      ?>
   </body>
   <script type="text/javascript">
     document.addEventListener("DOMContentLoaded", function() {
-      PageHandler.InitializeBanner(["../img/perthday.png"], ["../img/perthnight.png"]);
+      PageHandler.InitializeBanner(["img/perthday.png"], ["img/perthnight.png"]);
     }, false);
   </script>
 </html>
