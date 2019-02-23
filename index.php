652f2c47458202f2e6678298c8a07355892e72aa
James Noonan <warped.demon.prince@hotmail.com>
2019-02-23 14:11:39 +0800

    Update index.php

--- index.php
+++ index.php
@@ -35,15 +35,22 @@
           </pre>
 
           <pre>
-          Nelgara Pty Ltd is an Australian commercial management consulting firm that leverages its significant industry experience to develop and apply best practice solutions to a broad mix of service industries including oil and gas, water treatment, power distribution, infrastructure, resources and mining for both private and public institutions.
+          Nelgara Pty Ltd is an Australian commercial management consulting firm
+          that leverages its significant industry experience to develop and apply
+          best practice solutions to a broad mix of service industries including oil and gas,
+          water treatment, power distribution, infrastructure, resources and mining
+          for both private and public institutions.
           </pre>
 
           <pre>
-          We can understand our clients’ needs quickly, as we have worked there, and respond with significant and cost-effective improvements to their operational performance whilst supporting their business goals.
+          We can understand our clients’ needs quickly, as we have worked there,
+          and respond with significant and cost-effective improvements to their
+          operational performance whilst supporting their business goals.
           </pre>
 
           <pre>
-          We bring in combined local and global knowledge for the benefit of the Australian construction industry.
+          We bring in combined local and global knowledge for the benefit of the Australian
+          construction industry.
           </pre>
 
           <pre>
@@ -51,16 +58,23 @@
           </pre>");
 
         $MainContentHandlerHelper->CreateNewContent("cheese", "<b>Expertise</b>", "
-          <p>
-          Nelgara Pty Ltd brings more than 100 years of experience in delivering solutions across the end to end phases of projects both pre and post construction. Nelgara continues to draw on its actual delivery experience integrated with the latest technology solutions to provide a unique firm that delivers sustainable outcomes for an ever-changing world.
-          We are Skilled in Project Leadership, People, Schedule and Cost Management, Sub-Contractor Management, and Stressed Project Recovery (more consultants are joining monthly).
-          </p>");
+          <pre>
+          Nelgara Pty Ltd brings more than 100 years of experience in delivering
+          solutions across the end to end phases of projects both pre and post
+          construction. Nelgara continues to draw on its actual delivery experience
+          integrated with the latest technology solutions to provide a unique
+          firm that delivers sustainable outcomes for an ever-changing world.
+          We are Skilled in Project Leadership, People, Schedule and Cost Management,
+          Sub-Contractor Management, and Stressed Project Recovery (more consultants are joining monthly).
+          </pre>");
 
         $MainContentHandlerHelper->CreateNewContent("cheese", "<b>Locations</b>", "
-          <p>
-          Nelgara Pty Ltd is based in Perth, WA but consultants have both worked on projects and or being responsible at a business Contracts Manager or Commercial Manager level for projects in Queensland, NSW and Victoria.
+          <pre>
+          Nelgara Pty Ltd is based in Perth, WA but consultants have both worked on projects
+          and or being responsible at a business Contracts Manager or Commercial Manager level
+          for projects in Queensland, NSW and Victoria.
           We can also provide solutions through our network of consultant partners in the industry.
-          </p>");
+          </pre>");
     ?>
     <?php include("Php/Footer.php");
      ?>
