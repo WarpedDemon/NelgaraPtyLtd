c3b3ea9c50a578a8acc15fc507099c6da216dd8d
WarpedDemon <warped.demon.prince@hotmail.com>
2019-03-08 00:32:23 +0800

    Terms added

--- Php/Footer.php
+++ Php/Footer.php
@@ -8,7 +8,7 @@
       WA 6025<br/><br/>
       TEL: +61 (0)434 33 161<br/><br/>
       E-Mail: Nelgara@outlook.com<br/><br/></span></a>
-    <a href="#"><span id="Links">Terms & Conditions</span></a>
+    <a href="../terms.php"><span id="Links">Terms & Conditions</span></a>
   </div>
 
   ';
