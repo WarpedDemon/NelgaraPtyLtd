9bd773197a5ac39ba132cb0ac482224421a6380e
James Noonan <warped.demon.prince@hotmail.com>
2021-03-28 18:47:30 +0800

    update

--- Php/Footer.php
+++ Php/Footer.php
@@ -1,18 +1,13 @@
 <?php
 
   echo '
-  <div id="Footer">
+  <div id="Footer" style="color:black;">
     <a href="About.php"><span id="ContactInfo">Executive Headquarters</a>
-    <br/><br/>
     28 Henderson Drive
-    <br/><br/>
     Kallaroo, Perth
-    <br/><br/>
     WA 6025
-    <br/><br/>
     <a style="color:white;" href="tel:+6143433161">TEL: +61 (0)434 33 161</a>
-    <br/><br/>
-    E-Mail: Nelgara@outlook.com<br/><br/></span>
+    E-Mail: Nelgara@outlook.com
     <a href="../terms.php"><span id="Links">Terms & Conditions</span></a>
   </div>
 
