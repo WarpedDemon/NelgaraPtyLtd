c71a9c25972b0bc14f766609a648591285d7e67b
James Noonan <warped.demon.prince@hotmail.com>
2021-03-29 22:09:56 +0800

    Styling & Footer Update
    Making the backgrounds and footer less hideous.

--- Php/Footer.php
+++ Php/Footer.php
@@ -1,28 +1,59 @@
 <?php
 
   echo '
-  <div id="Footer" style="color:black;">
-    <div>
-      <p id="FooterText">
-        <a href="About.php"><span id="ContactInfo">Executive Headquarters</a>
-        <br></br>
-        28 Henderson Drive
-        <br></br>
-        Kallaroo, Perth
-        <br></br>
-        WA 6025
-        <br></br>
-        <a style="color:white;" href="tel:+6143433161">TEL: +61 (0)434 33 161</a>
-        <br></br>
-        E-Mail: Nelgara@outlook.com
-      </p>
+  <div id="Footer">
+
+    <div class="column">
+      <div class="row">
+        <img src="img/logo.png" style="margin-top: 8px;"></img>
+      </div>
+      <div class="row">
+        <!--middle-->
+      </div>
+      <div class="row">
+      </div>
     </div>
-    <div>
-      <a href="../terms.php"><span id="Links">Terms & Conditions</span></a>
+
+    <div class="column">
+      <div class="row">
+        <h2><a href="index.php" style="text-decoration: none; color: black;"> Nelgara Pty Ltd </a></h2>
+      </div>
+      <div class="row">
+        <!--middle-->
+        <h6 class="copyrightText">Copyright &#169 Nelgara Pty Ltd 2021 </h6>
+      </div>
+      <div class="row">
+        <a href="terms.php" id="terms">Terms and Conditions</a>
+      </div>
     </div>
+
+    <div class="column">
+      <div class="row">
+      </div>
+      <div class="row">
+       <!--middle-->
+        <a style="color:black;" href="tel:+6143433161">TEL: +61 (0)434 33 161</a>
+      </div>
+      <div class="row">
+      </div>
+    </div>
+
+
   </div>
 
   ';
 
 
 ?>
+<!--<p id="FooterText">
+<a href="About.php"><span id="ContactInfo">Executive Headquarters</a>
+<br></br>
+28 Henderson Drive
+<br></br>
+Kallaroo, Perth
+<br></br>
+WA 6025
+<br></br>
+<a style="color:white;" href="tel:+6143433161">TEL: +61 (0)434 33 161</a>
+<br></br>
+E-Mail: Nelgara@outlook.com-->
--- Php/Header.php
+++ Php/Header.php
@@ -2,7 +2,8 @@
 echo '
 
     <div id="Header">
-      <img src="../img/logo.png" id="Logo"></img>
+      <a id="top"></a>
+      <img src="img/logo.png" id="Logo"></img>
       <a id="LightSwitch" class=""><span class="fas fa-lightbulb"></span></a>
       <h1 id="Title" class=""><a href="index.php" id="Title_Text" class=""> Nelgara Pty Ltd </a></h1>
       <div id="Nav_Container" class="">
@@ -15,19 +16,6 @@ echo '
           <a href="../People.php" id="People"><li class="animate fadeInLeft wow" data-wow-delay="1s"><b><p>People</p></b></li></a>
         </ul>
     </div>
-    <!--
-    <div id="HeaderDark" class="HeaderClass animated fadeInDown">
-      <a id="LightSwitchDark" class="LightSwitchClass"><span class="fas fa-lightbulb"></span></a>
-      <h1 id="TitleDark" class="TitleClass"><a href="index.php" id="Title_TextDark" class="Title_TextClass"> Nelgara Pty Ltd </a></h1>
-      <div id="Nav_ContainerDark" class="Nav_ContainerClass">
-      <span id="HamburgerDark" style="color:white;" class="HamburgerClass fa fa-bars"></span>
-      </div>
-        <ul id="NavDark" class="NavClass">
-          <a href="../Services.php" id="ServicesDark" class="ServicesClass"><li class="animate fadeInLeft wow" data-wow-delay="0.25s"><b><p>Services</p></b></li></a>
-          <a href="../Industries.php" id="IndustriesDark" class="IndustriesClass"><li class="animate fadeInLeft wow" data-wow-delay="0.50s"><b><p>Industries</p></b></li></a>
-          <a href="../About.php" id="AboutDark" class="AboutClass"><li class="animate fadeInLeft wow" data-wow-delay="0.75s"><b><p>About</p></b></li></a>
-          <a href="../People.php" id="PeopleDark" class="PeopleClass"><li class="animate fadeInLeft wow" data-wow-delay="1s"><b><p>People</p></b></li></a>
-        </ul>
-    </div>-->
+
 ';
 ?>
